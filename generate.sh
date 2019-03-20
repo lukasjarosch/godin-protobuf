f3c18a79e0fdb26bc3e23689b6bec0066d47670f
Lukas Jarosch <lukas.jarosch@mail.com>
2019-03-20 19:18:08 +0100

    basic generate script working
    Signed-off-by: Lukas Jarosch <lukas.jarosch@mail.com>

--- generate.sh
+++ generate.sh
@@ -1,27 +1,31 @@
 #!/usr/bin/env bash
 
-# example for greeter, to be refined
+# This script creates go stubs of all proto files. It will also generate the HTTP gateway as well as the swagger spec
 
-# generate GO stubs from proto file
-protoc \
-		-I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway \
-    -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-    --proto_path=. \
-		--go_out=plugins=grpc:. \
-		./greeter/greeter.proto
+PROTO_ROOT=$(pwd)
 
-# generate grpc HTTP gateway from proto file
-protoc \
-		-I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway \
-    -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-		--proto_path=. \
-    --grpc-gateway_out=logtostderr=true:. \
-     ./greeter/greeter.proto
+for dir in $PROTO_ROOT/*/ ; do
+    echo -n "=> '$(basename ${dir})' ... "
 
-# generate swagger spec of the generated HTTP gateway
-protoc \
-    -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway \
-    -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-		--proto_path=. \
-    --swagger_out=logtostderr=true:. \
-    ./greeter/greeter.proto
+    pushd $dir > /dev/null
+
+    protoc -I$GOPATH/src \
+        -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway \
+        -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+        --proto_path=. --go_out=plugins=grpc:. *.proto
+
+    mkdir -p $dir/gateway
+    protoc -I$GOPATH/src \
+        -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway \
+        -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+        --proto_path=. --grpc-gateway_out=logtostderr=true:./gateway/ *.proto
+
+    mkdir -p $dir/swagger
+    protoc -I$GOPATH/src \
+        -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway \
+        -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+        --proto_path=. --swagger_out=logtostderr=true:./swagger/ *.proto
+    echo "DONE!"
+
+    popd > /dev/null
+done
