05f9332bdd083174ee7c65748f53f99e4959d5f7
Lukas Jarosch <lukas.jarosch@mail.com>
2019-03-20 21:19:29 +0100

    protoc is now called from repo-root and uses find to fetch all proto files recursively
    Signed-off-by: Lukas Jarosch <lukas.jarosch@mail.com>

--- generate.sh
+++ generate.sh
@@ -1,23 +1,27 @@
 #!/usr/bin/env bash
 
 # This script creates go stubs of all proto files. It will also generate the HTTP gateway as well as the swagger spec
+# TODO: make recursive. Currently
 
 PROTO_ROOT=$(pwd)
+FILES=$(find ${PROTO_ROOT} -type f -name "*.proto")
 
-for dir in $PROTO_ROOT/*/ ; do
-    echo -n "=> '$(basename ${dir})' ... "
+for proto in $FILES ; do
+    #echo -n "=> '$(basename ${dir})' ... "
 
-    pushd $dir > /dev/null
+    #pushd $dir > /dev/null
+
+    echo -n "+ ${proto} ..."
 
     protoc -I$GOPATH/src \
         -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway \
         -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-        --proto_path=. \
+        --proto_path=${PROTO_ROOT} \
         --go_out=paths=source_relative,plugins=grpc:. \
         --grpc-gateway_out=logtostderr=true,paths=source_relative:. \
         --swagger_out=logtostderr=true:. \
-        *.proto
+        "$proto"
     echo "DONE!"
 
-    popd > /dev/null
+    #popd > /dev/null
 done
