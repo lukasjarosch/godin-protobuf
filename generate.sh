a4a859b748553115bbb591e603e49f7d2dd8e38f
Lukas Jarosch <lukas.jarosch@mail.com>
2019-03-20 21:20:25 +0100

    cleanup
    Signed-off-by: Lukas Jarosch <lukas.jarosch@mail.com>

--- generate.sh
+++ generate.sh
@@ -1,18 +1,11 @@
 #!/usr/bin/env bash
 
 # This script creates go stubs of all proto files. It will also generate the HTTP gateway as well as the swagger spec
-# TODO: make recursive. Currently
 
 PROTO_ROOT=$(pwd)
 FILES=$(find ${PROTO_ROOT} -type f -name "*.proto")
 
 for proto in $FILES ; do
-    #echo -n "=> '$(basename ${dir})' ... "
-
-    #pushd $dir > /dev/null
-
-    echo -n "+ ${proto} ..."
-
     protoc -I$GOPATH/src \
         -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway \
         -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
@@ -22,6 +15,4 @@ for proto in $FILES ; do
         --swagger_out=logtostderr=true:. \
         "$proto"
     echo "DONE!"
-
-    #popd > /dev/null
 done
