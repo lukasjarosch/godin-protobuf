dbe3092eb94269c1d34e5e928579a2cc513ba442
Travis CI <travis@travis-ci.org>
2019-03-29 18:14:58 +0100

    update build script with proper GH_TOKEN
    Signed-off-by: Travis CI <travis@travis-ci.org>

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-REPO="git@github.com:lukasjarosch/godin-api-go"
+REPO="https://${GH_TOKEN}@github.com/lukasjarosch/godin-api-go.git"
 REPO_PATH="$(pwd)/godin-api-go"
 
 GEN_PATH="$(pwd)/gen/go"
@@ -16,16 +16,23 @@ function copyStubs() {
 	cp -R $GEN_PATH/* $REPO_PATH/
 }
 
+function setupGit() {
+	git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
 function commitAndPush() {
 	enterDir $1
+
+	setupGit
 
 	git add -N .
 
 	if ! git diff --no-ext-diff --quiet --exit-code; then
 		git add .
 		git status
-		git commit -m "Automatic generation of Go protobuf stubs"
-		git push origin HEAD
+		git commit -m "[$(git rev-parse --short HEAD)]: Automatic generation of Go protobuf stubs"
+		git push
 	else
 		echo "No changes detected for $1"
 	fi
@@ -37,6 +44,7 @@ function clone() {
 	rm -rf "$REPO_PATH"
 	echo "Cloning into repo: $REPO"
 	git clone $REPO "$REPO_PATH"
+	git fetch
 }
 
 # Helper for adding a directory to the stack and echoing the result
